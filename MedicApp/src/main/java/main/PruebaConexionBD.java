41b37d03d70fc2d7ad4d93955d053c64e003aac4
JuanSantos0298 <jukrolso@gmail.com>
2021-11-28 00:11:49 -0600

    Singleton

---
+++ MedicApp/src/main/java/main/PruebaConexionBD.java
@@ -0,0 +1,47 @@
+package main;
+
+/**
+ *
+ * @author Juan Santos
+ */
+
+//Importamos las librerias necesarias
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PruebaConexionBD {
+
+    public static void main(String[] args) {
+        try {
+            ConexionBD conexion = new ConexionBD();
+            Connection conecta = conexion.getConnection(); 
+
+            String msg = "Se ha establecido la conexion con la base de datos. :D"; 
+
+            System.out.println(msg);
+
+            String user = "Carlos Juan";
+            int id = 10000;
+
+            //Pruebas
+            String SQL = "Select nombre from paciente where id_paciente = '"+id+"';";
+            conexion.resultado = conexion.sentencia.executeQuery(SQL);
+            System.out.println("Registro encontrado. ");
+            
+            //
+            String sql = "Select * from paciente where id_paciente = '"+id+"';";
+            ResultSet data = conexion.sentencia.executeQuery(sql);
+            
+            while(data.next()){
+                System.out.println(data.getString("id_paciente") + " " + data.getString("nombre") + " " + data.getString("apellido_paterno") + " " + data.getString("apellido_materno") + " " + data.getString("direccion"));
+            }
+            
+        }catch(SQLException ex){
+            
+            System.out.println(ex);
+        }
+    }
+}
